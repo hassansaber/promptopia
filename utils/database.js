f10a25a6c2ed4d48adb4f02a409962508f9bf8e7
hassansaber <hassansaberkarin@gmail.com>
2023-11-11 10:50:55 +0330

    Create model

---
+++ utils/database.js
@@ -0,0 +1,27 @@
+import mongoose, { mongo } from "mongoose";
+
+let isConnected = false //track the connection
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true) // for no error
+
+  if (isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'Share_prompt',
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+    isConnected = true
+    console.log('MongoDB connected');
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
